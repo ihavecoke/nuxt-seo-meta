3d981eebe60db870bf4085983dd8cb5d4f5d79dd
ihavecoke <ihavecoke@163.com>
2021-01-09 01:22:51 +0800

    chore: update webpack config to covert typescript to js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
-// const path = require("path")
-//
-// module.exports = {
-//   devtool: false,
-//   entry: {
-//     plugin: "./src/plugin.ts"
-//   },
-//   mode: "production",
-//   output: {
-//     filename: "[name].js",
-//     libraryTarget: "amd",
-//     path: path.resolve(__dirname, "src")
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.ts$/,
-//         use: "ts-loader",
-//         exclude: /node_modules/
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: [".ts", ".js"]
-//   }
-// }
+const path = require("path")
+const ShellPlugin = require(path.resolve(__dirname, "shell-plugin.js"))
+
+module.exports = {
+  devtool: false,
+  entry: {
+    module: "./src/module.ts"
+  },
+  mode: "production",
+  output: {
+    filename: "[name].js",
+    libraryTarget: "commonjs",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [new ShellPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  node: {
+    __dirname: false
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: { path: require.resolve("path-browserify") }
+  }
+}
