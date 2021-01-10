621550d7aa97a948af127a7abb17c79ccddddb01
ihavecoke <ihavecoke@163.com>
2021-01-10 12:06:36 +0800

    feat: add more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,8 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-non-null-assertion": "off"
+    "import/order": "error",
+    "import/first": "error",
+    "import/no-mutable-exports": "error"
   }
 }
