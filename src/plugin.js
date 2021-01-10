7fcea22f124216ead5c2cdcabe600e978ab862d2
ihavecoke <ihavecoke@163.com>
2021-01-10 13:44:38 +0800

    feat: support pass second argument to control if auto inject metadata to html head tag

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,10 @@
-import seoMeta from './seo-meta'
-const defaultOptions = <%= serialize(options) %>
+import seoMeta from "./seo-meta"
 
+const defaultOptions = <%= serialize(options) %>
 export default (ctx, inject) => {
-  const fn = seoOptions => seoMeta({...defaultOptions, ...seoOptions}, ctx)
-  ctx.seoMeta = fn // to Compatible version <= 1.0.2
-  inject('seoMeta', fn)
+  const fn = (seoOptions, autoInjectToHead = true) => {
+    const context = autoInjectToHead ? ctx : {}
+    return seoMeta({ ...defaultOptions, ...seoOptions }, context)
+  }
+  inject("seoMeta", fn)
 }
