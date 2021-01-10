7de99bcc5e1e0759e01b7c1bef2bf8c778cb04b6
ihavecoke <ihavecoke@163.com>
2021-01-10 12:04:17 +0800

    test: support test seo-meta core logic

---
+++ src/seo-meta.ts
@@ -0,0 +1,91 @@
+// you can found more info: https://metatags.io/
+import merge from "lodash/merge"
+import keyBy from "lodash/keyBy"
+import values from "lodash/values"
+import { Context } from "@nuxt/types/app"
+
+interface ISeoMetaOptions {
+  title: string
+  description: string
+  url: string
+  image?: string
+  keywords?: string
+  desc?: string
+  locale?: string
+  siteName?: string
+  ignoreTwitter?: Boolean
+  twitterUser?: string
+  ignoreOG?: Boolean
+  defaultImage?: string
+  defaultUrl?: string
+  defaultDescription?: string
+}
+
+export default function seoMeta(
+  options: ISeoMetaOptions,
+  nuxtContext: Context = {} as Context
+) {
+  // default fallback
+  const imageUrl = options.image || options.defaultImage
+  const url = options.url || options.defaultUrl
+  const description = options.description || options.defaultDescription
+
+  const baseMeta = [
+    { name: "title", content: options.title },
+    { name: "description", content: description },
+    { name: "image", content: imageUrl },
+    { name: "keyword", content: options.keywords || description }
+  ]
+  // Facebook & LinkedIn
+  const ogMeta = [
+    { property: "og:title", content: options.title },
+    { property: "og:description", content: description },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: url },
+    { property: "og:image", content: imageUrl },
+    { property: "og:locale", content: options.locale },
+    { property: "og:site_name", content: options.siteName }
+  ]
+
+  const twitterMeta = [
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: url },
+    { name: "twitter:creator", content: options.twitterUser },
+    { name: "twitter:title", content: options.title },
+    { name: "twitter:description", content: description },
+    { name: "twitter:image", content: imageUrl }
+  ]
+
+  const metaTags: any = [baseMeta]
+  if (!options.ignoreTwitter) metaTags.push(twitterMeta)
+  if (!options.ignoreOG) metaTags.push(ogMeta)
+
+  const normalizedMetas = metaTags.flat().reduce((memo: any, tag: any) => {
+    if (!tag.content) return memo
+    if (tag.name) {
+      memo.push({
+        hid: tag.name,
+        name: tag.name,
+        content: tag.content
+      })
+    } else if (tag.property) {
+      memo.push({
+        hid: tag.property,
+        property: tag.property,
+        content: tag.content
+      })
+    }
+    return memo
+  }, [])
+
+  if (nuxtContext.app) {
+    const { app: nuxtApp } = nuxtContext
+    nuxtApp.head.meta = [
+      ...nuxtApp.head.meta,
+      ...values(
+        merge(keyBy(normalizedMetas, "hid"), keyBy(normalizedMetas, "hid"))
+      )
+    ]
+  }
+  return normalizedMetas
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,105 +1,105 @@
-// import seoMeta from "./seo-meta"
-//
-// const title = "title"
-// const description = "description"
-// const url = "https://url.com"
-// const baseOptions = {
-//   title,
-//   description,
-//   url,
-//   ignoreTwitter: true,
-//   ignoreOG: true
-// }
-//
-// test("should return default meta attributes", () => {
-//   const result = seoMeta({ defaultImage: "default.png", ...baseOptions })
-//   const expectValue = [
-//     { hid: "title", name: "title", content: "title" },
-//     { hid: "description", name: "description", content: "description" },
-//     { hid: "image", name: "image", content: "default.png" },
-//     { hid: "keyword", name: "keyword", content: "description" }
-//   ]
-//   expect(result).toStrictEqual(expectValue)
-// })
-//
-// test("should return twitter meta if not ignore twitter meta", () => {
-//   const result = seoMeta({ ...baseOptions, ignoreTwitter: false })
-//   const expectValue = [
-//     { hid: "title", name: "title", content: "title" },
-//     { hid: "description", name: "description", content: "description" },
-//     { hid: "keyword", name: "keyword", content: "description" },
-//     {
-//       hid: "twitter:card",
-//       name: "twitter:card",
-//       content: "summary_large_image"
-//     },
-//     {
-//       hid: "twitter:site",
-//       name: "twitter:site",
-//       content: "https://url.com"
-//     },
-//     { hid: "twitter:title", name: "twitter:title", content: "title" },
-//     {
-//       hid: "twitter:description",
-//       name: "twitter:description",
-//       content: "description"
-//     }
-//   ]
-//   expect(result).toStrictEqual(expectValue)
-// })
-//
-// test("should return og meta info if not ignore og", () => {
-//   const result = seoMeta({ ...baseOptions, ignoreOG: false })
-//   const expectValue = [
-//     { hid: "title", name: "title", content: "title" },
-//     { hid: "description", name: "description", content: "description" },
-//     { hid: "keyword", name: "keyword", content: "description" },
-//     { hid: "og:title", property: "og:title", content: "title" },
-//     {
-//       hid: "og:description",
-//       property: "og:description",
-//       content: "description"
-//     },
-//     { hid: "og:type", property: "og:type", content: "website" },
-//     { hid: "og:url", property: "og:url", content: "https://url.com" }
-//   ]
-//   expect(result).toStrictEqual(expectValue)
-// })
-//
-// test("should return all support meta", () => {
-//   const result = seoMeta({
-//     ...baseOptions,
-//     ignoreOG: false,
-//     ignoreTwitter: false
-//   })
-//   const expectValue = [
-//     { hid: "title", name: "title", content: "title" },
-//     { hid: "description", name: "description", content: "description" },
-//     { hid: "keyword", name: "keyword", content: "description" },
-//     {
-//       hid: "twitter:card",
-//       name: "twitter:card",
-//       content: "summary_large_image"
-//     },
-//     {
-//       hid: "twitter:site",
-//       name: "twitter:site",
-//       content: "https://url.com"
-//     },
-//     { hid: "twitter:title", name: "twitter:title", content: "title" },
-//     {
-//       hid: "twitter:description",
-//       name: "twitter:description",
-//       content: "description"
-//     },
-//     { hid: "og:title", property: "og:title", content: "title" },
-//     {
-//       hid: "og:description",
-//       property: "og:description",
-//       content: "description"
-//     },
-//     { hid: "og:type", property: "og:type", content: "website" },
-//     { hid: "og:url", property: "og:url", content: "https://url.com" }
-//   ]
-//   expect(result).toStrictEqual(expectValue)
-// })
+import seoMeta from "./../src/seo-meta"
+
+const title = "title"
+const description = "description"
+const url = "https://url.com"
+const baseOptions = {
+  title,
+  description,
+  url,
+  ignoreTwitter: true,
+  ignoreOG: true
+}
+
+test("should return default meta attributes", () => {
+  const result = seoMeta({ defaultImage: "default.png", ...baseOptions })
+  const expectValue = [
+    { hid: "title", name: "title", content: "title" },
+    { hid: "description", name: "description", content: "description" },
+    { hid: "image", name: "image", content: "default.png" },
+    { hid: "keyword", name: "keyword", content: "description" }
+  ]
+  expect(result).toStrictEqual(expectValue)
+})
+
+test("should return twitter meta if not ignore twitter meta", () => {
+  const result = seoMeta({ ...baseOptions, ignoreTwitter: false })
+  const expectValue = [
+    { hid: "title", name: "title", content: "title" },
+    { hid: "description", name: "description", content: "description" },
+    { hid: "keyword", name: "keyword", content: "description" },
+    {
+      hid: "twitter:card",
+      name: "twitter:card",
+      content: "summary_large_image"
+    },
+    {
+      hid: "twitter:site",
+      name: "twitter:site",
+      content: "https://url.com"
+    },
+    { hid: "twitter:title", name: "twitter:title", content: "title" },
+    {
+      hid: "twitter:description",
+      name: "twitter:description",
+      content: "description"
+    }
+  ]
+  expect(result).toStrictEqual(expectValue)
+})
+
+test("should return og meta info if not ignore og", () => {
+  const result = seoMeta({ ...baseOptions, ignoreOG: false })
+  const expectValue = [
+    { hid: "title", name: "title", content: "title" },
+    { hid: "description", name: "description", content: "description" },
+    { hid: "keyword", name: "keyword", content: "description" },
+    { hid: "og:title", property: "og:title", content: "title" },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: "description"
+    },
+    { hid: "og:type", property: "og:type", content: "website" },
+    { hid: "og:url", property: "og:url", content: "https://url.com" }
+  ]
+  expect(result).toStrictEqual(expectValue)
+})
+
+test("should return all support meta", () => {
+  const result = seoMeta({
+    ...baseOptions,
+    ignoreOG: false,
+    ignoreTwitter: false
+  })
+  const expectValue = [
+    { hid: "title", name: "title", content: "title" },
+    { hid: "description", name: "description", content: "description" },
+    { hid: "keyword", name: "keyword", content: "description" },
+    {
+      hid: "twitter:card",
+      name: "twitter:card",
+      content: "summary_large_image"
+    },
+    {
+      hid: "twitter:site",
+      name: "twitter:site",
+      content: "https://url.com"
+    },
+    { hid: "twitter:title", name: "twitter:title", content: "title" },
+    {
+      hid: "twitter:description",
+      name: "twitter:description",
+      content: "description"
+    },
+    { hid: "og:title", property: "og:title", content: "title" },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: "description"
+    },
+    { hid: "og:type", property: "og:type", content: "website" },
+    { hid: "og:url", property: "og:url", content: "https://url.com" }
+  ]
+  expect(result).toStrictEqual(expectValue)
+})
