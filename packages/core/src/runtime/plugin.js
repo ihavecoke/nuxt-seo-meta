b0f90eff68e8fed2d2108e66ecd770b59c4a5795
Mixbo <lb563@foxmail.com>
2022-02-26 22:48:30 +0800

    feat: add $metaInfos context method to generate meta info

--- packages/core/src/runtime/plugin.js
+++ packages/core/src/runtime/plugin.js
@@ -1,9 +1,12 @@
 import seoMeta from "nuxt-seo-meta/runtime"
+import { metaInfos } from "nuxt-seo-meta/dist/runtime/metainfo"
 import requrl from 'requrl'
 
 const defaultOptions = <%= serialize(options) %>
 
 export default (ctx, inject) => {
+
+  // auto generate meta tags and inject to html head
   const fn = (seoOptions) => {
     const defaultUrl = process.server ? requrl(ctx.req, true) : requrl(/* uses url */)
     const options = {
@@ -14,4 +17,16 @@ export default (ctx, inject) => {
     return seoMeta(options, ctx)
   }
   inject("seoMeta", fn)
+
+  // generate raw meta tags, this will return meta info objects
+  const metaInfosFn = (seoOptions) =>{
+    const defaultUrl = process.server ? requrl(ctx.req, true) : requrl(/* uses url */)
+    const options = {
+      ...defaultOptions,
+      url: defaultUrl,
+      ...seoOptions
+    }
+    return metaInfos(options)
+  }
+  inject("metaInfo", metaInfosFn)
 }
