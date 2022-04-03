b71e117435e717bcceeb1a8d72cd050d1649a459
Mixbo <lb563@foxmail.com>
2022-04-04 00:51:08 +0800

    feat: update demo url

--- packages/demo/pages/index.vue
+++ packages/demo/pages/index.vue
@@ -6,7 +6,7 @@
       <ul>
         <li>you can see the browser tab title 'SEO Meta - Title'</li>
         <li>you can open devtool element tab and search the &lt;head&gt; tag. more &lt;meta&gt; info generate for you.</li>
-        <li>change the title or more meta info read <a href="https://github.com/ihavecoke/nuxt-seo-meta" target="_blank">this</a></li>
+        <li>change the title or more meta info read <a href="https://github.com/ihavecoke/nuxt-seo-meta#support-config-options" target="_blank">this</a></li>
       </ul>
     </div>
   </div>
